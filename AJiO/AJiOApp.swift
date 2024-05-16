371051bfaac58a951b1b9d38f179df640e36d352
MnStan <mmaks97@gmail.com>
2024-05-16 20:17:12 +0200

    Initial Commit

---
+++ AJiO/AJiOApp.swift
@@ -0,0 +1,17 @@
+//
+//  AJiOApp.swift
+//  AJiO
+//
+//  Created by Maksymilian Stan on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AJiOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
