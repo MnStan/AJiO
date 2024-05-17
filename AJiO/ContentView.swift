29467936fd2ff822e4f243a175b95eb4f3d0d348
MnStan <mmaks97@gmail.com>
2024-05-17 20:41:47 +0200

    Added near voivodeship searching in nearest 100km

--- AJiO/ContentView.swift
+++ AJiO/ContentView.swift
@@ -10,19 +10,65 @@ import MapKit
 
 struct ContentView: View {
     @StateObject var locationManager = LocationManager()
+    @State var isShowingMarkers = false
+    @State var counter = 1
+    @State var isLoading = false
     
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
             Map(position: $locationManager.cameraPosition) {
                 UserAnnotation()
+                if isShowingMarkers {
+                    ForEach(locationManager.locations) { location in
+                        Annotation("Searching", coordinate: location.coordinate) {
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color.teal)
+                                .frame(width: 25, height: 25)
+                        }
+                    }
+                }
             }
             
-            Text(locationManager.state ?? "Unknowned")
-                .padding()
-                .background(.secondary)
-                .clipShape(.capsule)
-                .padding(25)
+            VStack {
+                Text(locationManager.state ?? "Unknowned")
+                    .padding()
+                    .background(.secondary)
+                    .clipShape(.capsule)
+                    .padding(25)
+                    .onTapGesture {
+                        print(locationManager.locations)
+                        isShowingMarkers.toggle()
+                        print("COUNT", locationManager.locations.count)
+                        print(locationManager.$nearVoivodeships)
+                        
+                    }
+                
+                if locationManager.isLoadingNearVoivodeships {
+                    RoundedRectangle(cornerRadius: 10)
+                         .fill(Color.blue)
+                         .frame(width: 80, height: 20)
+                         .offset(x: isLoading ? -50 : 50)
+                         .animation(.easeInOut.repeatForever(), value: isLoading)
+                         .onAppear {
+                             self.isLoading = true
+                         }
+                         .padding()
+                                
+                } else {
+                    Text("\(locationManager.nearVoivodeships)")
+                }
+            }
         }
+        .alert("Wystąpił problem", isPresented: $locationManager.shouldShowThrottledError) {
+            Button("Tak") {
+                locationManager.getPointsVoivodeshipsAgain()
+                isLoading = false
+            }
+            Button("Nie", role: .cancel) { }
+        } message: {
+            Text("Osiągnięto limit zapytań do wyszukiwania najbliższych województw.\nCzy chcesz pobrać je ponownie?")
+        }
+
     }
 }
 
--- AJiO/LocationManager.swift
+++ AJiO/LocationManager.swift
@@ -7,12 +7,15 @@
 
 import SwiftUI
 import MapKit
+import CoreLocation
+
+struct LocationData: Identifiable {
+    let id = UUID()
+    let coordinate: CLLocationCoordinate2D
+}
 
 final class LocationManager: NSObject, ObservableObject {
     private let locationManager = CLLocationManager()
-    private let geocoder = CLGeocoder()
-    private let circleRadius: CLLocationDistance = 1000
-    private let numberOfPointsOnCircle = 5
     
     @Published var cameraPosition = MapCameraPosition.region(MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 50.049683, longitude: 19.944544),
@@ -22,6 +25,14 @@ final class LocationManager: NSObject, ObservableObject {
     @Published var userLocation: CLLocationCoordinate2D?
     @Published var state: String?
     @Published private var surroundingVoivodeships: [String] = []
+    @Published var locations: [LocationData] = []
+    @Published var nearVoivodeships: Set<String> = []
+    @Published var isLoadingNearVoivodeships = true
+    @Published var shouldShowThrottledError = false
+    
+    private let geocoder = CLGeocoder()
+    private let radius: CLLocationDistance = 100000 // 100 km
+    private let numberOfPoints = 20
     
     override init() {
         super.init()
@@ -31,7 +42,7 @@ final class LocationManager: NSObject, ObservableObject {
         self.setup()
     }
     
-    func setup() {
+    private func setup() {
         switch locationManager.authorizationStatus {
         case .authorizedWhenInUse:
             locationManager.requestLocation()
@@ -43,23 +54,91 @@ final class LocationManager: NSObject, ObservableObject {
         }
     }
     
-    func getVoivodeship(from location: CLLocation){
-        geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
-            guard error == nil else {
-                print("Error retrieving location info: \(error!.localizedDescription)")
-                return
+    private func getVoivodeship(from location: CLLocation, completion: @escaping (String?) -> Void) {
+        geocoder.reverseGeocodeLocation(location) { [weak self] (placemarks, error) in
+            guard let self else { return }
+            if error != nil {
+                self.shouldShowThrottledError = true
+                nearVoivodeships.removeAll()
+                completion(nil)
             }
             
-            if let placemark = placemarks?.first {
-                if let state = placemark.administrativeArea {
-                    self.state = state
-                } else {
-                    self.state = "Nie można odczytać województwa"
-                }
+            if let placemark = placemarks?.first, placemark.country == "Polska" {
+                let voivodeship = placemark.administrativeArea
+                completion(voivodeship)
             } else {
-                self.state = "Nie można odczytać województwa"
+                completion(nil)
             }
         }
+    }
+
+    private func getPointsVoivodeships(from points: [LocationData], completion: @escaping ([String]) -> Void) {
+        let dispatchGroup = DispatchGroup()
+        var voivodeships: [String] = []
+
+        var delay: TimeInterval = 0.0
+        for point in points {
+            var leaveDispatchGroup = true
+            
+            dispatchGroup.enter()
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+                guard let self = self else { return }
+                self.getVoivodeship(from: CLLocation(latitude: point.coordinate.latitude, longitude: point.coordinate.longitude)) { voivodeship in
+                    if let voivodeship = voivodeship {
+                        if voivodeship != self.state {
+                            voivodeships.append(voivodeship)
+                        }
+                    }
+                    
+                    if leaveDispatchGroup {
+                        dispatchGroup.leave()
+                        leaveDispatchGroup = false
+                    }
+                }
+            }
+            delay += 0.5
+        }
+        
+        dispatchGroup.notify(queue: .main) { [weak self] in
+            guard let self else { return }
+            self.isLoadingNearVoivodeships = false
+            completion(voivodeships)
+        }
+    }
+    
+    func getPointsVoivodeshipsAgain() {
+        isLoadingNearVoivodeships = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 60) { [weak self] in
+            guard let self else { return }
+            self.getPointsVoivodeships(from: self.locations) { voivodeships in
+                self.nearVoivodeships = Set(voivodeships)
+                print(self.nearVoivodeships)
+            }
+        }
+    }
+    
+    private func pointsOnCircle(center: CLLocationCoordinate2D, radius: CLLocationDistance, numberOfPoints: Int) {
+        var points: [LocationData] = []
+        let earthRadius: CLLocationDistance = 6371000
+        
+        for i in 0..<numberOfPoints {
+            let bearing = Double(i) * 360.0 / Double(numberOfPoints) * .pi / 180
+            
+            let lat1 = center.latitude * .pi / 180
+            let lon1 = center.longitude * .pi / 180
+            
+            let lat2 = asin(sin(lat1) * cos(radius / earthRadius) + cos(lat1) * sin(radius / earthRadius) * cos(bearing))
+            var lon2 = lon1 + atan2(sin(bearing) * sin(radius / earthRadius) * cos(lat1), cos(radius / earthRadius) - sin(lat1) * sin(lat2))
+            
+            lon2 = (lon2 + 3 * .pi).truncatingRemainder(dividingBy: 2 * .pi) - .pi
+            
+            let lat2Degrees = lat2 * 180 / .pi
+            let lon2Degrees = lon2 * 180 / .pi
+            
+            points.append(LocationData(coordinate: CLLocationCoordinate2D(latitude: lat2Degrees, longitude: lon2Degrees)))
+        }
+        
+        self.locations = points
     }
 }
 
@@ -70,18 +149,31 @@ extension LocationManager: CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Something went wrong: \(error)")
+        print("Something went wrong: \(error.localizedDescription)")
     }
     
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        locations.last.map {
+        locations.last.map { location in
             cameraPosition = MapCameraPosition.region(MKCoordinateRegion(
-                center: $0.coordinate,
+                center: location.coordinate,
                 span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
             ))
+            userLocation = location.coordinate
             
-            userLocation = $0.coordinate
-            getVoivodeship(from: CLLocation(latitude: $0.coordinate.latitude, longitude: $0.coordinate.longitude))
+            getVoivodeship(from: CLLocation(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)) { [weak self] voivodeship in
+                guard let self else { return }
+                self.state = voivodeship
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+                guard let self else { return }
+                pointsOnCircle(center: location.coordinate, radius: radius, numberOfPoints: numberOfPoints)
+                getPointsVoivodeships(from: self.locations) { voivodeships in
+                    self.nearVoivodeships = Set(voivodeships)
+                    print(self.nearVoivodeships)
+                }
+            }
         }
     }
 }
